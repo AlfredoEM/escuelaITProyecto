fa142803d54ea7e72786ffd8408ce9470a82244d
Alfredo Espiritu M <EspirituMAlfredo@gmail.com>
2015-03-31 09:40:29 -0500

    Segundo commit

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <body>
 
 <h1>Hola mundo desde Github</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sed iusto incidunt eius, facilis est. Tenetur architecto accusamus, omnis, quos incidunt ab. Nihil expedita in reiciendis repellendus id veritatis praesentium.</p>
 	
 </body>
 </html>
